c1226f65eb0fe4d224a8e5158f8d7a06078d4023
Harald Gliebe <harald.gliebe@gmail.com>
2017-03-08 10:55:21 +0100

    tests for DefaultLoginService

--- src/test/java/org/dhis/security/LoginServiceTest.java
+++ src/test/java/org/dhis/security/LoginServiceTest.java
@@ -1,14 +1,16 @@
 package org.dhis.security;
 
-import org.dhis.security.DefaultLoginService;
-import org.dhis.security.LoginService;
+import org.dhis.user.User;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LoginServiceTest
 {
     private LoginService loginService;
-    
+
+    private static final User TEST_USER = new User("John");
+    private static final AuthenticationEvent TEST_AUTHENTICATION_EVENT = new AuthenticationEvent(TEST_USER.getUsername());
     @Before
     public void before()
     {
@@ -16,8 +18,31 @@ public class LoginServiceTest
     }
     
     @Test
-    public void testFoo()
+    public void testInitiallyNotBlocked()
     {
-        // TODO Implement at least two unit tests verifying the LoginService interface behavior
+        Assert.assertTrue("User should not be blocked initially", loginService.isBlocked(TEST_USER));
+    }
+
+    @Test
+    public void testLoginBlockedAfter5Failures()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            loginService.registerAuthenticationFailure(TEST_AUTHENTICATION_EVENT);
+        }
+        Assert.assertFalse("User should be blocked after 5 failures", loginService.isBlocked(TEST_USER));
+    }
+
+
+    @Test
+    public void testFailureCounterIsReset()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            loginService.registerAuthenticationFailure(TEST_AUTHENTICATION_EVENT);
+        }
+        loginService.registerAuthenticationSuccess(TEST_AUTHENTICATION_EVENT);
+        loginService.registerAuthenticationFailure(TEST_AUTHENTICATION_EVENT);
+        Assert.assertTrue("Failure count should be reset after one successful login", loginService.isBlocked(TEST_USER));
     }
 }
