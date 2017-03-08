d66a1aa4daae8ae7bb9eefa09759c491a6422421
Harald Gliebe <harald.gliebe@gmail.com>
2017-03-08 10:54:50 +0100

    implemented DefaultLoginService

--- src/main/java/org/dhis/security/DefaultLoginService.java
+++ src/main/java/org/dhis/security/DefaultLoginService.java
@@ -1,35 +1,46 @@
 package org.dhis.security;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.dhis.user.User;
 
 import com.github.benmanes.caffeine.cache.LoadingCache;
+
+import java.util.concurrent.TimeUnit;
 
 public class DefaultLoginService
     implements LoginService
 {
     /**
      * Cache for login attempts where usernames are keys and login attempts are values.
+     * Entries expire after 1 hour after the last write to reset the login attempts.
+     *
+     * <p> However this has the following implications: <ul>
+     * <li>After 5 failed attempts a user is only blocked for 1 hour and not permanently</li>
+     * <li>If a user makes an 5 attempts with a 59 minute break between each of them,
+     *    he will still be blocked although he only did 2 failed attempts within each 1 hour timeframe
+     *    Implementing the requirement exactly would require a more  sophisticated implementation that
+     *    keeps track of the timestamps of the login attempts</li></ul>
      */
-    private final LoadingCache<String, Integer> USERNAME_LOGIN_ATTEMPTS_CACHE = null;
-    
-    // TODO Instantiate and configure this cache (https://github.com/ben-manes/caffeine)
-    
+    private final LoadingCache<String, Integer> USERNAME_LOGIN_ATTEMPTS_CACHE =
+            Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.HOURS).build(username -> 0);
+
     @Override
     public void registerAuthenticationFailure( AuthenticationEvent event )
     {
-        // TODO Implement this method        
+        // it is important to increment the login attempts atomically using the ConcurrentMap.compute method,
+        // otherwise concurrent brute force attacks could  create race conditions that allow more then 5 attempts
+        USERNAME_LOGIN_ATTEMPTS_CACHE.asMap().compute(event.getUsername(), (__, count) -> count != null ? count + 1 : 1);
     }
 
     @Override
     public void registerAuthenticationSuccess( AuthenticationEvent event )
     {
-        // TODO Implement this method        
+        USERNAME_LOGIN_ATTEMPTS_CACHE.invalidate(event.getUsername());
     }
 
     @Override
     public boolean isBlocked( User user )
     {
-        // TODO Implement this method        
-        return false;
+        return USERNAME_LOGIN_ATTEMPTS_CACHE.get(user.getUsername()) < 5;
     }
 }
